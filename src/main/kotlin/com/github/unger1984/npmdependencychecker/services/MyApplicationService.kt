4d5d1e2b2a1a71a3554637c4ea5cbc766690b120
GitHub Action <action@github.com>
2022-01-23 08:36:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/unger1984/npmdependencychecker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.unger1984.npmdependencychecker.services
+
+import com.github.unger1984.npmdependencychecker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/unger1984/npmdependencychecker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/unger1984/npmdependencychecker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.unger1984.npmdependencychecker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.unger1984.npmdependencychecker.MyBundle
 
 class MyProjectService(project: Project) {
 
